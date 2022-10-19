4b513dcf8216669573b684ac6e590452ed1d0239
Amine Elmouradi <elmouraa.pro@gmail.com>
2022-10-19 19:33:24 +0200

    Add create todo endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+let id = 0;
+
 interface Todo {
   id: number;
   text: string;
@@ -10,20 +14,43 @@ interface Todo {
 
 const todos: Todo[] = [
   {
-    id: 1,
+    id: 0,
     text: "Learn nodejs",
     isDone: false,
   },
 ];
 
+const createTodo = (text: string) => {
+  const todo: Todo = {
+    id: ++id,
+    text,
+    isDone: false,
+  };
+
+  todos.push(todo);
+
+  return { ...todo };
+};
+
 // TODO: Sign up endpoint
 // TODO: Login endpoint
 
 // TODO: Create todo endpoint
+app.post("/todos/create", (req, res) => {
+  const body = req.body;
+
+  if (!body?.text) {
+    res.status(400).send("text property is undefined");
+    return;
+  }
+
+  const todo = createTodo(body.text);
+  res.json(todo);
+});
 // TODO: Update todo endpoint
 // TODO: Delete todo endpoint
 
-app.get("/todos", (req, res) => {
+app.get("/todos", (_, res) => {
   res.json(todos);
 });
 
