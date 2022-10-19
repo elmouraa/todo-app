2dddd127e7ad22114a2e771fd89fdca76db2cc6f
Amine Elmouradi <elmouraa.pro@gmail.com>
2022-10-19 19:21:18 +0200

    Add list all todos endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,36 @@ import express from "express";
 
 const app = express();
 
+interface Todo {
+  id: number;
+  text: string;
+  isDone: boolean;
+}
+
+const todos: Todo[] = [
+  {
+    id: 1,
+    text: "Learn nodejs",
+    isDone: false,
+  },
+];
+
+// TODO: Sign up endpoint
+// TODO: Login endpoint
+
+// TODO: Create todo endpoint
+// TODO: Update todo endpoint
+// TODO: Delete todo endpoint
+
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
+
 app.get("/hello", (req, res) => {
   res.send("Hello world");
 });
 
-app.listen(3001, () => {
+// TODO: Use env to store PORT
+app.listen(3000, () => {
   console.log("Server running at localhost:3001");
 });
